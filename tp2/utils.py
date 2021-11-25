b0daf3b75c0caa06e6ea1c6977dfa93a90a9de27
nassimb0u <in_boulechfar@esi.dz>
2021-11-25 11:40:00 +0100

    Feat group libvirt util in class

--- tp2/utils.py
+++ tp2/utils.py
@@ -1,25 +1,20 @@
 import libvirt
 
-def connect(uri: str=None) -> libvirt.virConnect:
-    try:
-        return libvirt.openReadOnly(uri)
-    except libvirt.libvirtError:
-        return None
+class LibVirtApi:
+    def __init__(self, uri: str=None) -> None:
+        self.conn = libvirt.openReadOnly(uri)
 
-def close_conn(conn: libvirt.virConnect):
-    conn.close()
+    def get_hyper_name(self) -> str:
+        return self.conn.getHostname()
 
-def get_hyper_name() -> str:
-    return 
+    def ls_vms(self) -> list[str]:
+        return ["vm1", "vm2", "vm3"]
 
-def ls_vms() -> list[str]:
-    return ["vm1", "vm2", "vm3"]
+    def start_vm(self, vm: str) -> bool:
+        return True
 
-def start_vm(vm: str) -> bool:
-    return True
+    def stop_vm(self, vm: str) -> bool:
+        return False
 
-def stop_vm(vm: str) -> bool:
-    return False
-
-def get_vm_info(vm: str) -> str:
-    return "info of"
+    def get_vm_info(self, vm: str) -> str:
+        return "info of"
